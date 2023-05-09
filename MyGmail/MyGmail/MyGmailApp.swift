a1b1ea0422a90a0b113f3dcbe66b46b36b0e1beb
Simon Zhang <szhang2378@gmail.com>
2023-05-08 21:26:10 -0500

    First Commit

---
+++ MyGmail/MyGmail/MyGmailApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyGmailApp.swift
+//  MyGmail
+//
+//  Created by Shuai Zhang on 5/8/23.
+//
+
+import SwiftUI
+
+@main
+struct MyGmailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
